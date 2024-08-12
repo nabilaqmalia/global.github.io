7bcc9ab6a891ccee59704d9aa99c2aeecac5464f
nabilaqmalia <nabilaaqmalia6@gmail.com>
2024-08-12 13:54:26 +0700

    first commit

---
+++ proses.php
@@ -0,0 +1,12 @@
+<?php
+$koneksi = mysqli_connect("localhostt","root","","contact");
+
+// $nama = $_POST ['nama'];
+// $email = $_POST ['email'];
+// $pesan = $_POST ['pesan'];
+
+// $query = "INSERT INTO contact VALUES ('$nama','$email','$pesan')";
+
+// mysqli_query($koneksi,$query);
+// echo"pesan terkirim";
+?>
